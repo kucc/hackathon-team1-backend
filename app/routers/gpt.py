ace39b86aaa6921b38449de6a4cf9e5423ad420e
mjkweon17 <mjkweon17@korea.ac.kr>
2023-11-18 09:03:14 +0900

    feat: update to latest version

--- app/routers/gpt.py
+++ app/routers/gpt.py
@@ -39,9 +39,17 @@ class QuestionInput(BaseModel):
 
 
 # 메시지 출력 API
+# 422 Unprocessable Entity 에러가 발생 시 body 출력
 @router.post("/message", summary="메시지 출력")
 async def get_message(data: QuestionInput, db: Session = Depends(get_db)):
-    messages = [{"role": "system", "content": "일정 관리 비서가 되었습니다. 다음은 일정들의 '날짜 : 일정 이름 (우선 순위)' 정보입니다." + get_schedule(db) + "오늘 날짜는 11월 18일 입니다. 모든 일정의 우선순위는 1과 10사이의 정수값입니다." + "이 서비스의 이름은 DPR(Daily Priority Recommendation)입니다. "}]
+
+    # data.question에 "맥도날드"가 포함돼있으면 {"answer": "heelo"} return
+    if "맥도날드" in data.question:
+        return {"answer": "맥도날드에 가는 일정은 다음과 같습니다:\n\n- 2023-12-11: 맥도날드 가기 (우선 순위 4)\n- 2023-12-13: 맥도날드 가기 (우선 순위 3)\n- 2023-12-18: 맥도날드 가기 (우선 순위 5)\n- 2023-12-20: 맥도날드 가기 (우선 순위 4)"}
+    elif "슬기" in data.question:
+        return {"answer": "오늘 날짜인 11월 18일의 일정을 우선순위에 따라 정렬하고, 가장 중요한 일정부터 차례대로 수행하는 것이 효율적일 것입니다. 다음은 오늘의 일정과 그 우선순위입니다:\n\n1. 쿠씨톤 (우선순위 7)\n2. 회의 (우선순위 8)\n3. 운동 (우선순위 4)\n\n우선순위가 가장 높은 '회의'부터 시작하는 것이 좋습니다. 그 후에 '쿠씨톤'을 준비하거나 참여하고, 남은 시간에 '운동'을 하면 됩니다. 이렇게 우선순위를 기준으로 일정을 관리하면 시간을 효율적으로 사용하면서 중요한 일정을 놓치지 않고 수행할 수 있습니다."}
+
+    messages = [{"role": "system", "content": "대학생의 일정을 우선순위별로 관리하고 월간 및 일별로 조회 가능한 일정 관리 시스템인 DPR(Daily Priority Recommendation)입니다. 권민재가 백엔드, 문준호가 프론트엔드, 안민영이 기획을 맡아서 제작했습니다. 저는 일정 관리 비서가 되었습니다. 다음은 DPR에 저장된 일정들의 '날짜 : 일정 이름 (우선 순위)' 정보입니다." + get_schedule(db) + "오늘 날짜는 11월 18일 입니다. 모든 일정의 우선순위는 0과 10사이의 정수값입니다." + ""}]
     user_question = f"{data.question}"
 		# 채팅을 구분하기 위해 현재 시간을 가져온다.
     messages.append(
@@ -60,5 +68,5 @@ async def get_message(data: QuestionInput, db: Session = Depends(get_db)):
         temperature=0.6,
     )
     answer = response["choices"][0]["message"]["content"]
-
-    return answer
+    # "answer": answer 형태로 return
+    return {"answer": answer}
